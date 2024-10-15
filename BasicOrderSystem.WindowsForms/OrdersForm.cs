551003b5a51c6da3653312203c5bddb77983dcac
hugh19se <hugh19s@icloud.com>
2024-10-15 10:40:35 -0700

    Added Windows Forms project

---
+++ BasicOrderSystem.WindowsForms/OrdersForm.cs
@@ -0,0 +1,21 @@
+﻿using BasicOrderSystem.WebClients;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicOrderSystem.WindowsForms
+{
+    public partial class OrdersForm : Form
+    {
+        public OrdersForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
