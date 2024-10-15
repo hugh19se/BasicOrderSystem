8efe6d7552dab720e09a26d54546e845abc4003d
hugh19se <hugh19s@icloud.com>
2024-10-15 13:29:05 -0700

    Added logic to OrdersForm

--- BasicOrderSystem.WindowsForms/OrdersForm.Designer.cs
+++ BasicOrderSystem.WindowsForms/OrdersForm.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "OrdersForm";
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            FromLabel = new Label();
+            ToLabel = new Label();
+            StatusComboBox = new ComboBox();
+            StatusLabel = new Label();
+            SearchButton = new Button();
+            OrdersListView = new ListView();
+            OrderIDHeader = new ColumnHeader();
+            CustomerIDHeader = new ColumnHeader();
+            StatusHeader = new ColumnHeader();
+            TotalHeader = new ColumnHeader();
+            OrderPlacedHeader = new ColumnHeader();
+            OrderDeliveredHeader = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 27);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(218, 27);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 1;
+            // 
+            // FromLabel
+            // 
+            FromLabel.AutoSize = true;
+            FromLabel.Location = new Point(12, 9);
+            FromLabel.Name = "FromLabel";
+            FromLabel.Size = new Size(35, 15);
+            FromLabel.TabIndex = 2;
+            FromLabel.Text = "From";
+            // 
+            // ToLabel
+            // 
+            ToLabel.AutoSize = true;
+            ToLabel.Location = new Point(218, 9);
+            ToLabel.Name = "ToLabel";
+            ToLabel.Size = new Size(20, 15);
+            ToLabel.TabIndex = 3;
+            ToLabel.Text = "To";
+            // 
+            // StatusComboBox
+            // 
+            StatusComboBox.FormattingEnabled = true;
+            StatusComboBox.Location = new Point(424, 27);
+            StatusComboBox.Name = "StatusComboBox";
+            StatusComboBox.Size = new Size(93, 23);
+            StatusComboBox.TabIndex = 4;
+            // 
+            // StatusLabel
+            // 
+            StatusLabel.AutoSize = true;
+            StatusLabel.Location = new Point(424, 9);
+            StatusLabel.Name = "StatusLabel";
+            StatusLabel.Size = new Size(39, 15);
+            StatusLabel.TabIndex = 5;
+            StatusLabel.Text = "Status";
+            // 
+            // SearchButton
+            // 
+            SearchButton.Location = new Point(523, 26);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(75, 23);
+            SearchButton.TabIndex = 6;
+            SearchButton.Text = "Search";
+            SearchButton.UseVisualStyleBackColor = true;
+            SearchButton.Click += SearchButton_Click;
+            // 
+            // OrdersListView
+            // 
+            OrdersListView.Columns.AddRange(new ColumnHeader[] { OrderIDHeader, CustomerIDHeader, StatusHeader, TotalHeader, OrderPlacedHeader, OrderDeliveredHeader });
+            OrdersListView.FullRowSelect = true;
+            OrdersListView.Location = new Point(12, 56);
+            OrdersListView.Name = "OrdersListView";
+            OrdersListView.Size = new Size(776, 226);
+            OrdersListView.TabIndex = 7;
+            OrdersListView.UseCompatibleStateImageBehavior = false;
+            OrdersListView.View = View.Details;
+            // 
+            // OrderIDHeader
+            // 
+            OrderIDHeader.Text = "Order ID";
+            // 
+            // CustomerIDHeader
+            // 
+            CustomerIDHeader.Text = "Customer ID";
+            CustomerIDHeader.Width = 80;
+            // 
+            // StatusHeader
+            // 
+            StatusHeader.Text = "Status";
+            StatusHeader.Width = 70;
+            // 
+            // TotalHeader
+            // 
+            TotalHeader.Text = "Order Total";
+            TotalHeader.Width = 80;
+            // 
+            // OrderPlacedHeader
+            // 
+            OrderPlacedHeader.Text = "Order Placed";
+            OrderPlacedHeader.Width = 145;
+            // 
+            // OrderDeliveredHeader
+            // 
+            OrderDeliveredHeader.Text = "Order Delivered";
+            OrderDeliveredHeader.Width = 145;
+            // 
+            // OrdersForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(OrdersListView);
+            Controls.Add(SearchButton);
+            Controls.Add(StatusLabel);
+            Controls.Add(StatusComboBox);
+            Controls.Add(ToLabel);
+            Controls.Add(FromLabel);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Name = "OrdersForm";
+            Text = "Orders";
+            Load += OrdersForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label FromLabel;
+        private Label ToLabel;
+        private ComboBox StatusComboBox;
+        private Label StatusLabel;
+        private Button SearchButton;
+        private ListView OrdersListView;
+        private ColumnHeader OrderIDHeader;
+        private ColumnHeader CustomerIDHeader;
+        private ColumnHeader StatusHeader;
+        private ColumnHeader TotalHeader;
+        private ColumnHeader OrderPlacedHeader;
+        private ColumnHeader OrderDeliveredHeader;
     }
 }
--- BasicOrderSystem.WindowsForms/OrdersForm.cs
+++ BasicOrderSystem.WindowsForms/OrdersForm.cs
@@ -1,10 +1,14 @@
-﻿using BasicOrderSystem.WebClients;
+﻿using BasicOrderSystem.Models;
+using BasicOrderSystem.WebClients;
+using BasicOrderSystem.WebContracts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,9 +17,73 @@ namespace BasicOrderSystem.WindowsForms
 {
     public partial class OrdersForm : Form
     {
+        private string? AppBaseAddress;
+        private OrdersClient OrdersClient;
+
         public OrdersForm()
         {
+            //Check if API Base Address is populated before orders form can open
+            AppBaseAddress = ConfigurationManager.AppSettings["OrderAPIBaseAddress"];
+            while (string.IsNullOrEmpty(AppBaseAddress))
+            {
+                BaseAddressPromptForm baseAddressPromptForm = new();
+                baseAddressPromptForm.ShowDialog();
+                AppBaseAddress = ConfigurationManager.AppSettings["OrderAPIBaseAddress"];
+            }
+            OrdersClient = new OrdersClient(AppBaseAddress);
+
             InitializeComponent();
+        }
+        private void OrdersForm_Load(object sender, EventArgs e)
+        {
+            //Populate statuses combo box
+            foreach (OrderStatuses status in Enum.GetValues(typeof(OrderStatuses)))
+            {
+                StatusComboBox.Items.Add(status);
+            }
+        }
+        private async void SearchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check if user has selected a status
+                if (string.IsNullOrEmpty(StatusComboBox.Text))
+                {
+                    MessageBox.Show("Please Enter A Valid Status", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //Make API call
+                GetOrdersResponse? response = await OrdersClient.GetOrdersAsync();
+                IList<Order> orders = response.Orders;
+
+                //Iterate over orders, add to orderListViewItems
+                List<ListViewItem> orderListViewItems = new();
+                foreach (Order order in orders)
+                {
+                    ListViewItem orderItem = new();
+
+                    orderItem.Text = order.OrderID.ToString();
+                    orderItem.SubItems.Add(order.CustomerID.ToString());
+                    orderItem.SubItems.Add(((OrderStatuses)order.Status).ToString());
+                    orderItem.SubItems.Add("£" + order.Total.ToString());
+                    orderItem.SubItems.Add(order.OrderPlaced.ToString());
+                    orderItem.SubItems.Add(order.OrderDelivered.ToString());
+
+                    orderListViewItems.Add(orderItem);
+                }
+
+                //Add new items to OrdersListView
+                OrdersListView.BeginUpdate();
+                OrdersListView.Items.Clear();
+                OrdersListView.Items.AddRange(orderListViewItems.ToArray());
+                OrdersListView.EndUpdate();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Add Serilog
+                MessageBox.Show("An Error Occurred While Sending The API Request:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
