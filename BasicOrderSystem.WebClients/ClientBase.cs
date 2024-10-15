87cae97d9785b71a5c1b7a0933d15a485a05568c
hugh19se <hugh19s@icloud.com>
2024-10-15 10:39:11 -0700

    Added WebClients project

---
+++ BasicOrderSystem.WebClients/ClientBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace BasicOrderSystem.WebClients
+{
+    public class ClientBase : IDisposable
+    {
+        private readonly HttpClient HttpClient;
+
+        protected ClientBase(string baseAddress, int timeout = 60)
+        {
+            HttpClient = GetHttpClient(baseAddress, timeout);
+        }
+
+        private static HttpClient GetHttpClient(string baseAddress, int timeout)
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        protected async Task<TResponseType> GetResponse<TResponseType>(string path)
+        {
+            HttpResponseMessage? response = await HttpClient.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+
+            Stream? stream = await response.Content.ReadAsStreamAsync();
+            TResponseType? result = JsonSerializer.Deserialize<TResponseType>(stream);
+
+            return result;
+        }
+
+        public void Dispose()
+        { 
+            HttpClient.Dispose();
+        }
+    }
+}
---
+++ BasicOrderSystem.WebClients/OrdersClient.cs
@@ -0,0 +1,27 @@
+﻿using BasicOrderSystem.WebContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOrderSystem.WebClients
+{
+    public class OrdersClient : ClientBase
+    {
+        private readonly string ControllerRoute = "/api/Orders/";
+
+        public OrdersClient(string baseAddress, int timeout = 60)
+            : base(baseAddress, timeout){}
+
+        public async Task<GetCustomersResponse> GetCustomersAsync()
+        {
+            return await GetResponse<GetCustomersResponse>(ControllerRoute + "GetCustomers");
+        }
+
+        public async Task<GetOrdersResponse> GetOrdersAsync()
+        {
+            return await GetResponse<GetOrdersResponse>(ControllerRoute + "GetOrders");
+        }
+    }
+}
