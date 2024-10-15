c2df8c1f89d001dd282fb8cdae24ee6ec3b4fcc1
hugh19se <hugh19s@icloud.com>
2024-10-15 10:40:12 -0700

    Added correct Json Property Names for models and web contracts

--- BasicOrderSystem.WebContracts/GetCustomersResponse.cs
+++ BasicOrderSystem.WebContracts/GetCustomersResponse.cs
@@ -1,10 +1,12 @@
 ﻿using BasicOrderSystem.Models;
 using BasicOrderSystem.WebContracts.Bases;
+using System.Text.Json.Serialization;
 
 namespace BasicOrderSystem.WebContracts
 {
     public class GetCustomersResponse : ResponseBase
     {
+        [JsonPropertyName("customers")]
         public IList<Customer> Customers { get; set; }
     }
 }
--- BasicOrderSystem.WebContracts/GetOrdersResponse.cs
+++ BasicOrderSystem.WebContracts/GetOrdersResponse.cs
@@ -1,10 +1,12 @@
 ﻿using BasicOrderSystem.Models;
 using BasicOrderSystem.WebContracts.Bases;
+using System.Text.Json.Serialization;
 
 namespace BasicOrderSystem.WebContracts
 {
     public class GetOrdersResponse : ResponseBase
     {
+        [JsonPropertyName("orders")]
         public IList<Order> Orders { get; set; }
     }
 }
