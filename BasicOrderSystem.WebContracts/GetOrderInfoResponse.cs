4cf7b2185b36d3690647f02f5b897b1b829fbb1b
hugh19se <hugh19s@icloud.com>
2024-10-22 18:57:05 -0700

    Added GetOrderInfoResponse

---
+++ BasicOrderSystem.WebContracts/GetOrderInfoResponse.cs
@@ -0,0 +1,14 @@
+﻿using BasicOrderSystem.Models;
+using BasicOrderSystem.WebContracts.Bases;
+using System.Text.Json.Serialization;
+
+namespace BasicOrderSystem.WebContracts
+{
+    public class GetOrderInfoResponse : ResponseBase
+    {
+        [JsonPropertyName("order")]
+        public Order Order { get; set; }
+        [JsonPropertyName("customer")]
+        public Customer Customer { get; set; }
+    }
+}
